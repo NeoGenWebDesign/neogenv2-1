b6c70cc7b6c42e2477185522a312db8532bf32db
NeoGenWebDesign <66866164+NeoGenWebDesign@users.noreply.github.com>
2026-01-27 20:50:00 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="neo-root">
       {/* Header / Hero Section */}
       <header className="neo-hero">
-        <img src="src/assets/neogenlogo2.png/" alt="neogen logo" className="neo-logo" />
+        <img src="assets/neogenlogo2.png/" alt="neogen logo" className="neo-logo" />
         <h1 className="neo-title">neogen web design</h1>
         <h2 className="neo-subtitle">Modern. Interactive. Intelligent.</h2>
         <div className="neo-socials">
